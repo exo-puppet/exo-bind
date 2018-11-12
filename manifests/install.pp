b1af7619b49d81c56c20bef16f0cd6dc29afd037
Frédéric DROUET <fdrouet@exoplatform.com>
2018-11-12 15:10:28 +0100

    ITOP-3959 : add missing apt-get update step

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,5 +2,9 @@
 #
 # This class manages the installation of the bind package and related utils
 class bind::install {
-  ensure_packages ('bind9', { 'ensure' => $bind::params::ensure_mode, 'name' => $bind::params::package_name })
+  ensure_packages ('bind9', {
+    'ensure' => $bind::params::ensure_mode,
+    'name' => $bind::params::package_name,
+    'require' => Class['apt::update']
+    })
 }
